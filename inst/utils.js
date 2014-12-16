6c21fb2a1a263973cea2c9c078b5c72d5c3cb52b
Winston Chang <winston@stdout.org>
2014-12-15 22:02:51 -0600

    Exit script on error

--- inst/utils.js
+++ inst/utils.js
@@ -14,3 +14,17 @@ exports.parseArgs = function(args) {
 
   return opts;
 };
+
+
+// Exit on error, instead of just hanging
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
