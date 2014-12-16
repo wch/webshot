38cdcc7efcc1269304fbe5762362266f76f29974
Winston Chang <winston@stdout.org>
2014-12-16 14:33:03 -0600

    Move JS error handler code

--- inst/screenshot.js
+++ inst/screenshot.js
@@ -87,3 +87,17 @@ function findClipRect(opts, page) {
     return null;
   }
 }
+
+
+// Exit on error, instead of just hanging
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
--- inst/utils.js
+++ inst/utils.js
@@ -23,16 +23,3 @@ exports.merge = function(a, b) {
   }
   return a;
 };
-
-// Exit on error, instead of just hanging
-phantom.onError = function(msg, trace) {
-  var msgStack = ['PHANTOM ERROR: ' + msg];
-  if (trace && trace.length) {
-    msgStack.push('TRACE:');
-    trace.forEach(function(t) {
-      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
-    });
-  }
-  console.error(msgStack.join('\n'));
-  phantom.exit(1);
-};
