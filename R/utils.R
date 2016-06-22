73e3b47dd5a93a4c55e89fff0b89a8ed071ae2bc
Winston Chang <winston@stdout.org>
2016-06-22 12:00:44 -0500

    Fix downloading issues

--- R/utils.R
+++ R/utils.R
@@ -47,11 +47,11 @@ find_phantom <- function() {
 #' @param baseURL The base URL for the location of PhantomJS binaries for
 #'   download. If the default download site is unavailable, you may specify an
 #'   alternative mirror, such as
-#'   \code{"https://github.com/paladox/phantomjs/releases/download/2.1.7/"}.
+#'   \code{"https://bitbucket.org/ariya/phantomjs/downloads/"}.
 #' @return \code{NULL} (the executable is written to a system directory).
 #' @export
 install_phantomjs <- function(version = '2.1.1',
-    baseURL = 'https://bitbucket.org/ariya/phantomjs/downloads/') {
+    baseURL = 'https://github.com/wch/webshot/releases/download/v0.3.1/') {
 
   if (!grepl("/$", baseURL))
     baseURL <- paste0(baseURL, "/")
@@ -60,27 +60,31 @@ install_phantomjs <- function(version = '2.1.1',
   on.exit(setwd(owd), add = TRUE)
   if (is_windows()) {
     zipfile <- sprintf('phantomjs-%s-windows.zip', version)
-    utils::download.file(paste0(baseURL, zipfile), zipfile, mode = 'wb')
+    download(paste0(baseURL, zipfile), zipfile, mode = 'wb')
     utils::unzip(zipfile)
     zipdir <- sub('.zip$', '', zipfile)
     exec <- file.path(zipdir, 'bin', 'phantomjs.exe')
   } else if (is_osx()) {
     zipfile <- sprintf('phantomjs-%s-macosx.zip', version)
-    utils::download.file(paste0(baseURL, zipfile), zipfile, mode = 'wb')
+    download(paste0(baseURL, zipfile), zipfile, mode = 'wb')
     utils::unzip(zipfile)
     zipdir <- sub('.zip$', '', zipfile)
     exec <- file.path(zipdir, 'bin', 'phantomjs')
     Sys.chmod(exec, '0755')  # chmod +x
-  } else {
+  } else if (is_linux()) {
     zipfile <- sprintf(
       'phantomjs-%s-linux-%s.tar.bz2', version,
       if (grepl('64', Sys.info()[['machine']])) 'x86_64' else 'i686'
     )
-    utils::download.file(paste0(baseURL, zipfile), zipfile, mode = 'wb')
+    download(paste0(baseURL, zipfile), zipfile, mode = 'wb')
     utils::untar(zipfile)
     zipdir <- sub('.tar.bz2$', '', zipfile)
     exec <- file.path(zipdir, 'bin', 'phantomjs')
     Sys.chmod(exec, '0755')  # chmod +x
+  } else {
+    # Unsupported platform, like Solaris
+    message("Sorry, this platform is not supported.")
+    return(invisible())
   }
   success <- FALSE
   dirs <- phantom_paths()
@@ -94,7 +98,7 @@ install_phantomjs <- function(version = '2.1.1',
     'Unable to install PhantomJS to any of these dirs: ',
     paste(dirs, collapse = ', ')
   )
-  message('phantomjs.exe has been installed to ', normalizePath(destdir))
+  message('phantomjs has been installed to ', normalizePath(destdir))
   invisible()
 }
 
@@ -122,6 +126,7 @@ dropNulls <- function(x) {
 
 is_windows <- function() .Platform$OS.type == "windows"
 is_osx <- function() Sys.info()[['sysname']] == 'Darwin'
+is_linux <- function() Sys.info()[['sysname']] == 'Linux'
 
 # Find an available TCP port (to launch Shiny apps)
 available_port <- function(port) {
@@ -136,4 +141,87 @@ available_port <- function(port) {
   }
   if (is.null(port)) stop("Cannot find an available port")
   port
+}
+
+# Wrapper for utils::download.file which works around a problem with R 3.3.0 and
+# 3.3.1. In these versions, download.file(method="libcurl") issues a HEAD
+# request to check if a file is available, before sending the GET request. This
+# causes problems when downloading attached files from GitHub binary releases
+# (like the PhantomJS binaries), because the url for the GET request returns a
+# 403 for HEAD requests. See
+# https://stat.ethz.ch/pipermail/r-devel/2016-June/072852.html
+download <- function(url, destfile, mode = "w") {
+  if (getRversion() == "3.3.0" || getRversion() == "3.3.1") {
+    download_no_libcurl(url, destfile, mode = mode)
+
+  } else if (is_windows() && getRversion() < "3.2") {
+    # Older versions of R on Windows need setInternet2 to download https.
+    download_old_win(url, destfile, mode = mode)
+
+  } else {
+    utils::download.file(url, destfile, mode = mode)
+  }
+}
+
+
+# Adapted from downloader::download, but avoids using libcurl.
+download_no_libcurl <- function(url, ...) {
+  # Windows
+  if (is_windows()) {
+    method <- "wininet"
+    utils::download.file(url, method = method, ...)
+
+  } else {
+    # If non-Windows, check for libcurl/curl/wget/lynx, then call download.file with
+    # appropriate method.
+
+    if (nzchar(Sys.which("wget")[1])) {
+      method <- "wget"
+    } else if (nzchar(Sys.which("curl")[1])) {
+      method <- "curl"
+
+      # curl needs to add a -L option to follow redirects.
+      # Save the original options and restore when we exit.
+      orig_extra_options <- getOption("download.file.extra")
+      on.exit(options(download.file.extra = orig_extra_options))
+
+      options(download.file.extra = paste("-L", orig_extra_options))
+
+    } else if (nzchar(Sys.which("lynx")[1])) {
+      method <- "lynx"
+    } else {
+      stop("no download method found")
+    }
+
+    utils::download.file(url, method = method, ...)
+  }
+}
+
+
+# Adapted from downloader::download, for R<3.2 on Windows
+download_old_win <- function(url, ...) {
+  # If we directly use setInternet2, R CMD CHECK gives a Note on Mac/Linux
+  seti2 <- `::`(utils, 'setInternet2')
+
+  # Check whether we are already using internet2 for internal
+  internet2_start <- seti2(NA)
+
+  # If not then temporarily set it
+  if (!internet2_start) {
+    # Store initial settings, and restore on exit
+    on.exit(suppressWarnings(seti2(internet2_start)))
+
+    # Needed for https. Will get warning if setInternet2(FALSE) already run
+    # and internet routines are used. But the warnings don't seem to matter.
+    suppressWarnings(seti2(TRUE))
+  }
+
+  method <- "internal"
+
+  # download.file will complain about file size with something like:
+  #       Warning message:
+  #         In download.file(url, ...) : downloaded length 19457 != reported length 200
+  # because apparently it compares the length with the status code returned (?)
+  # so we supress that
+  utils::download.file(url, method = method, ...)
 }
