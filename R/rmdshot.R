d7ee4acbcf53ebf32211122e4beef784916f49e8
Barret Schloerke <schloerke@gmail.com>
2018-02-28 10:45:29 -0800

    add shiny rmdshot example

--- R/rmdshot.R
+++ R/rmdshot.R
@@ -15,8 +15,15 @@
 #'
 #' @examples
 #' if (interactive()) {
+#'   # rmdshot("rmarkdown_file.Rmd", "snapshot.png")
+#'
+#'   # R Markdown file
 #'   input_file <- system.file("examples/knitr-minimal.Rmd", package = "knitr")
 #'   rmdshot(input_file, "minimal_rmd.png")
+#'
+#'   # Shiny R Markdown file
+#'   input_file <- system.file("examples/shiny.Rmd", package = "webshot")
+#'   rmdshot(input_file, "shiny_rmd.png", delay = 5)
 #' }
 #'
 #' @export
