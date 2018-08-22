bb2a4486398e2fc1462d1069dd68ff2ddf294137
Winston Chang <winston@stdout.org>
2018-08-22 13:09:01 -0500

    Use updated version of register_s3_method

--- R/zzz.R
+++ R/zzz.R
@@ -1,34 +1,31 @@
-# Reusable function for registering a set of methods with S3 manually. The
-# methods argument is a list of character vectors, each of which has the form
-# c(package, genname, class).
-registerMethods <- function(methods) {
-  lapply(methods, function(method) {
-    pkg <- method[[1]]
-    generic <- method[[2]]
-    class <- method[[3]]
-    func <- get(paste(generic, class, sep="."))
-    if (pkg %in% loadedNamespaces()) {
-      registerS3method(generic, class, func, envir = asNamespace(pkg))
+register_s3_method <- function(pkg, generic, class, fun = NULL) {
+  stopifnot(is.character(pkg), length(pkg) == 1)
+  stopifnot(is.character(generic), length(generic) == 1)
+  stopifnot(is.character(class), length(class) == 1)
+
+  if (is.null(fun)) {
+    fun <- get(paste0(generic, ".", class), envir = parent.frame())
+  } else {
+    stopifnot(is.function(fun))
+  }
+
+  if (pkg %in% loadedNamespaces()) {
+    registerS3method(generic, class, fun, envir = asNamespace(pkg))
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(
+    packageEvent(pkg, "onLoad"),
+    function(...) {
+      registerS3method(generic, class, fun, envir = asNamespace(pkg))
     }
-    setHook(
-      packageEvent(pkg, "onLoad"),
-      function(...) {
-        registerS3method(generic, class, func, envir = asNamespace(pkg))
-      }
-    )
-  })
+  )
 }
 
+
 .onLoad <- function(...) {
-  # webshot provides methods for knitr::knit_print, but knitr isn't a Depends or
-  # Imports of htmltools, only an Enhances. Therefore, the NAMESPACE file has to
-  # declare it as an export, not an S3method. That means that R will only know to
-  # use our methods if webshot is actually attached, i.e., you have to use
-  # library(webshot) in a knitr document or else you'll get escaped HTML in your
-  # document. This code snippet manually registers our method(s) with S3 once both
-  # webshot and knitr are loaded.
-  registerMethods(list(
-    # c(package, genname, class)
-    c("knitr", "knit_print", "webshot")
-  ))
+  # webshot provides methods for knitr::knit_print, but knitr isn't a Depends
+  # or Imports of htmltools, only an Enhances. This code snippet manually
+  # registers our method(s) with S3 once both webshot and knitr are loaded.
+  register_s3_method("knitr", "knit_print", "webshot")
 }
