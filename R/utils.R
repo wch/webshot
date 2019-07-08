29ba528f227b456185cd5c2f3f18db7cccb0fdb9
James J Balamuta <coatless@users.noreply.github.com>
2019-07-08 10:38:05 -0500

    Add a space after common

--- R/utils.R
+++ R/utils.R
@@ -209,7 +209,7 @@ phantom_paths <- function() {
   } else {
     path <- '~/bin'
   }
-  path <- c(path, file.path(system.file(package = 'webshot'),'PhantomJS'))
+  path <- c(path, file.path(system.file(package = 'webshot'), 'PhantomJS'))
   path
 }
 
